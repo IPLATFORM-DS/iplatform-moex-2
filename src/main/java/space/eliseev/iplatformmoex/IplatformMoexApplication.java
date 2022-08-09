c180e5cb014b8785ad2b7cb5f4b16ea0140b4d58
eliseev <a.s.eliseev@yandex.ru>
2022-08-09 17:59:48 +0400

    Initial commit

---
+++ src/main/java/space/eliseev/iplatformmoex/IplatformMoexApplication.java
@@ -0,0 +1,13 @@
+package space.eliseev.iplatformmoex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IplatformMoexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IplatformMoexApplication.class, args);
+	}
+
+}
