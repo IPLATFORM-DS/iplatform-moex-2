4b3136f9663ac616cc6dbbf77869275bc507975f
Podnebesnii <gidronya@gmail.com>
2022-08-12 17:23:56 +0300

    IPM-28 Entity SecurityGroup

---
+++ src/main/java/space/eliseev/iplatformmoex/model/entity/SecurityGroup.java
@@ -0,0 +1,28 @@
+package space.eliseev.iplatformmoex.model.entity;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "security_group")
+public class SecurityGroup {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Integer id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "is_hidden")
+    private boolean isHidden;
+}
