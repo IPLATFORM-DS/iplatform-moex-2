e668a9557e90ef2c91625f7c4f778573bdaa4a6f
Dmitriy-Grakovich <gudvin-gora@mail>
2022-08-12 09:47:40 +0300

    addEntitySecurityCollection

---
+++ src/main/java/space/eliseev/iplatformmoex/models/SecurityCollection.java
@@ -0,0 +1,29 @@
+package space.eliseev.iplatformmoex.models;
+
+import lombok.*;
+import javax.persistence.*;
+
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "security_collection")
+public class SecurityCollection {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Integer id;
+
+    @Column(name = "name",unique = true)
+    private String name;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "security_group_id")
+    private Integer securityGroupId;
+
+}
