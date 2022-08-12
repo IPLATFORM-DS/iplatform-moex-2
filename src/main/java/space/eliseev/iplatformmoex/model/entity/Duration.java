363b063ba79e8c4c134d5cd23e88a39cc6fea941
Podnebesnii <gidronya@gmail.com>
2022-08-12 19:06:27 +0300

    Добавил IPM-29 Entity Duration

---
+++ src/main/java/space/eliseev/iplatformmoex/model/entity/Duration.java
@@ -0,0 +1,34 @@
+package space.eliseev.iplatformmoex.model.entity;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(name = "durations")
+
+public class Duration {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Integer id;
+
+    @Column(name = "interval")
+    private Integer interval;
+
+    @Column(name = "duration")
+    private Integer duration;
+
+    @Column(name = "days")
+    private Integer days;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "hint")
+    private String hint;
+}
