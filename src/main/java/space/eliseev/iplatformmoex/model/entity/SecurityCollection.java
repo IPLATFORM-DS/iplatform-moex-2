5e69aee6301c60ecc5f001b99a2f22bbb73fd9f7
QayTem <qasytqkn@gmail.com>
2022-08-16 11:55:37 +0500

    IPM-42 Написать скрипт на заполнение таблицы security_collection

--- src/main/java/space/eliseev/iplatformmoex/model/entity/SecurityCollection.java
+++ src/main/java/space/eliseev/iplatformmoex/model/entity/SecurityCollection.java
@@ -11,7 +11,12 @@ import javax.persistence.*;
 @Builder
 @Entity
 @Table(name = "security_collection")
-public class SecurityCollection extends BaseEntity {
+public class SecurityCollection {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Integer id;
 
     @Column(name = "name",unique = true)
     private String name;
