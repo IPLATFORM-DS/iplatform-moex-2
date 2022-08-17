300f0ed3c488b3c50a9e296f2a9734335f7d8019
theSofiChan <101165970+theSofiChan@users.noreply.github.com>
2022-08-17 07:44:45 +0300

    IPM-44-[iplatform-moex] Написать скрипт на создание таблицы "check_metadata" и реализовать entity

---
+++ src/main/java/space/eliseev/iplatformmoex/model/entity/CheckMetadata.java
@@ -0,0 +1,25 @@
+package space.eliseev.iplatformmoex.model.entity;//package space.eliseev.iplatformmoex.entity;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Entity
+@Table(name="check_metadata")
+public class CheckMetadata {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @Column(name="id")
+    private Long id;
+    @Column(name="timestamp")
+    private LocalDateTime timestamp;
+    @Column(name="index")
+    private String index;
+    @Column(name="is_valid")
+    private  Boolean isValid;
+}
