ba9d4bf9e239996ae5757322ffcbc1b0f571ec97
QayTem <qasytqkn@gmail.com>
2022-08-17 21:37:04 +0500

    IPM-33 entity Engine

---
+++ src/main/java/space/eliseev/iplatformmoex/model/entity/Engine.java
@@ -0,0 +1,27 @@
+package space.eliseev.iplatformmoex.model.entity;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "engines")
+public class Engine {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Integer id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "title")
+    private String title;
+}
+
+
+
+
