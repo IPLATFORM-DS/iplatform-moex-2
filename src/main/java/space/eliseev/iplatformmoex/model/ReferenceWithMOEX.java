ea1d57681f72bb469c77ae836617f4d05af76971
Dmitriy-Grakovich <gudvin-gora@mail>
2022-08-18 11:16:02 +0300

    KA-45_entityMOEXFeignClientVersion3

---
+++ src/main/java/space/eliseev/iplatformmoex/model/ReferenceWithMOEX.java
@@ -0,0 +1,37 @@
+package space.eliseev.iplatformmoex.model;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import space.eliseev.iplatformmoex.model.entity.*;
+
+import java.util.List;
+
+@NoArgsConstructor
+@Getter
+@Setter
+public class ReferenceWithMOEX {
+
+    private List<Duration> durations;
+
+    private List<SecurityType> securityTypes;
+
+    private List<SecurityGroup> securityGroups;
+
+    private List<SecurityCollection> securityCollections;
+
+    private List<BoardGroup> boardGroups;
+
+    private List<Engine> engines;
+
+    private List<Market> markets;
+
+    private List<Board> boards;
+
+
+
+
+
+
+
+}
--- src/main/java/space/eliseev/iplatformmoex/service/ReferenceWithMOEXService.java
+++ src/main/java/space/eliseev/iplatformmoex/service/ReferenceWithMOEXService.java
@@ -4,7 +4,7 @@ package space.eliseev.iplatformmoex.service;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import space.eliseev.iplatformmoex.model.entity.ReferenceWithMOEX;
+import space.eliseev.iplatformmoex.model.ReferenceWithMOEX;
 
 @FeignClient(value = "MOEX-api", url = "https://iss.moex.com/iss/")
 public interface ReferenceWithMOEXService {
