70b5afb8a30e3ec759177d50abf74f5afefc9a10
theSofiChan <101165970+theSofiChan@users.noreply.github.com>
2022-08-20 15:09:19 +0300

    Добавила Сервис

--- src/main/java/space/eliseev/iplatformmoex/service/EngineService.java
+++ src/main/java/space/eliseev/iplatformmoex/service/EngineService.java
@@ -5,10 +5,6 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import space.eliseev.iplatformmoex.model.entity.Engine;
-
-import java.util.LinkedHashMap;
-import java.util.List;
 
 @FeignClient(value = "engine",url="https://iss.moex.com/iss/")
 public interface EngineService {
