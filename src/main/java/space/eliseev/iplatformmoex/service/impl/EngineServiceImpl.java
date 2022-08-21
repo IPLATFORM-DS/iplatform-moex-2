a556212759e9573ee4d9d52352171b78d5483499
theSofiChan <101165970+theSofiChan@users.noreply.github.com>
2022-08-21 17:15:53 +0300

    Поправила сервис

--- src/main/java/space/eliseev/iplatformmoex/service/impl/EngineServiceImpl.java
+++ src/main/java/space/eliseev/iplatformmoex/service/impl/EngineServiceImpl.java
@@ -3,8 +3,14 @@ package space.eliseev.iplatformmoex.service.impl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.lang.Nullable;
 import org.springframework.stereotype.Service;
+import space.eliseev.iplatformmoex.model.entity.Engine;
 import space.eliseev.iplatformmoex.service.EngineService;
 import space.eliseev.iplatformmoex.service.client.EngineClient;
+
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -14,24 +20,20 @@ public class EngineServiceImpl implements EngineService {
 
     @Override
     public Object getEngines(@Nullable String lang) {
-        return engineClient.getEngines(lang);
-    }
+        LinkedHashMap<String, LinkedHashMap<String, LinkedHashMap<String, ArrayList<LinkedHashMap<String, String>>>>>
+                data = (LinkedHashMap<String, LinkedHashMap<String, LinkedHashMap<String,
+                ArrayList<LinkedHashMap<String, String>>>>>) engineClient.getEngines(lang);
+        List<Engine> engins = data.get("data").get("rows").get("row").stream().map(o -> {
+            Integer id = Integer.parseInt(o.get("id"));
+            String name = o.get("name");
+            String title = o.get("title");
+            Engine engine = new Engine();
+            engine.setId(id);
+            engine.setName(name);
+            engine.setTitle(title);
+            return engine;
 
-//    public List<Engine> engineList(@RequestParam(value = "lang", required = false) String lang) {
-//        LinkedHashMap all = (LinkedHashMap) engineClient.getEngines(lang);
-//        LinkedHashMap data = (LinkedHashMap) all.get("data");
-//        LinkedHashMap rows = (LinkedHashMap) data.get("rows");
-//        List<Object> row = (List<Object>) rows.get("row");
-//        List<Engine> engines = row.stream().map(o -> {
-//            Integer id = Integer.parseInt((String) ((LinkedHashMap<?, ?>) o).get("id"));
-//            String name = (String) ((LinkedHashMap<?, ?>) o).get("name");
-//            String title = (String) ((LinkedHashMap<?, ?>) o).get("title");
-//            Engine engine = new Engine();
-//            engine.setId(id);
-//            engine.setName(name);
-//            engine.setTitle(title);
-//            return engine;
-//        }).collect(Collectors.toList());
-//        return engines;
-//    }
+        }).collect(Collectors.toList());
+        return engins;
+    }
 }
