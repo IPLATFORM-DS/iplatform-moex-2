626643ab3fd5f34c3ecd8a896da14ef3c32599c7
theSofiChan <101165970+theSofiChan@users.noreply.github.com>
2022-08-26 20:40:12 +0300

    IPM-57 Реализовать сервис получения промежуточных итогов дня

--- src/main/java/space/eliseev/iplatformmoex/IplatformMoexApplication.java
+++ src/main/java/space/eliseev/iplatformmoex/IplatformMoexApplication.java
@@ -3,7 +3,6 @@ package space.eliseev.iplatformmoex;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.cloud.openfeign.FeignClient;
 
 @EnableFeignClients
 @SpringBootApplication
