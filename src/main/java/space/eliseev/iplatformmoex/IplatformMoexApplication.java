2e91e585028bc0b3927ca5ac314b614db29d7124
QayTem <qasytqkn@gmail.com>
2022-08-27 16:24:36 +0500

    IPM-53 Вынести все url в отельный файл

--- src/main/java/space/eliseev/iplatformmoex/IplatformMoexApplication.java
+++ src/main/java/space/eliseev/iplatformmoex/IplatformMoexApplication.java
@@ -2,13 +2,10 @@ package space.eliseev.iplatformmoex;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import space.eliseev.iplatformmoex.configuration.UrlConfig;
 
 @SpringBootApplication
 @EnableFeignClients
-@EnableConfigurationProperties(UrlConfig.class)
 public class IplatformMoexApplication {
 
     public static void main(String[] args) {
